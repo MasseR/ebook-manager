c45ea0ecc84adb7ed8d2c2cda399e04fff984ad9
Mats Rauhala <mats.rauhala@iki.fi>
2018-12-20 00:33:32 +0200

    wip

---
+++ backend/src/Configuration.hs
@@ -0,0 +1,26 @@
+{-# Language NoImplicitPrelude #-}
+{-# Language DeriveGeneric #-}
+{-# Language DuplicateRecordFields #-}
+module Configuration where
+
+import ClassyPrelude
+import Dhall (Interpret)
+
+data Pg = Pg { username :: Text
+             , password :: Text
+             , host :: Text
+             , database :: Text }
+        deriving (Show, Generic)
+
+data Store = Filestore { path :: Text }
+           | IPFS { common :: Text }
+           deriving (Show, Generic)
+
+data Config = Config { database :: Pg
+                     , store :: Store }
+            deriving (Show, Generic)
+
+instance Interpret Pg
+instance Interpret Store
+instance Interpret Config
+
